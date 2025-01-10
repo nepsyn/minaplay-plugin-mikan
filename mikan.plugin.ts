228511dc61a14d855b74b589d888209e24ff939b
Nepsyn <nepsyn@outlook.com>
2025-01-10 23:02:42 +0800

    chore: Update version 1.0.3

--- mikan.plugin.ts
+++ mikan.plugin.ts
@@ -9,7 +9,7 @@ const icon =
 @MinaPlayPlugin({
   id: 'mikan',
   icon,
-  version: '1.0.1',
+  version: '1.0.3',
   supportVersion: '^0.2.5',
   description: 'Mikan support in MinaPlay',
   author: 'MinaPlay',
